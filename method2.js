f9280cb2d76ef7cfdf05d9fbca6cc314ccc5df4e
omidka <okhub-github@yahoo.com>
2018-02-14 08:51:24 -0800

    - Fixed the typo in method2

--- method2.js
+++ method2.js
@@ -2,11 +2,10 @@ const {expect} = require('chai');
 var today = new Date();
 var month = today.getMonth() + 1; //January is 0!
 
-context(`Validation 2`, () = > {
-    it(`Today's Month is =` + month, () =
->
-{
-    expect(2).to.equal(month)
-}
-)
+context(`Validation 2`, () => {
+    it(`Today's Month is =` + month, () =>
+        {
+        expect(2).to.equal(month)
+        }
+    )
 })
